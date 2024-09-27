4871f025ea6d1a21e058179560312e11856c0a3e
Bùi Minh Quang <quangbmse171655@fpt.edu.vn>
2024-09-27 23:14:53 +0700

    update

--- CosmicAdventure2DGame/Assets/Sciptes/PlayerMoverment.cs
+++ CosmicAdventure2DGame/Assets/Sciptes/PlayerMoverment.cs
@@ -40,6 +40,7 @@ public class PlayerMovement : MonoBehaviour
         grounded = false;
     }
 
+
     private void OnCollisionEnter2D(Collision2D collision)
     {
         if(collision.gameObject.tag == "Ground")
